6417656527c11aa551b28100bb14f708052538f2
D9eka <120364200+D9eka@users.noreply.github.com>
2025-08-11 13:43:39 +0500

    Рефакторинг интерактивных элементов
    - Удаление наследования

--- Assets/Internal/Scripts/InteractableObjects/InteractableObject.cs
+++ Assets/Internal/Scripts/InteractableObjects/InteractableObject.cs
@@ -1,12 +1,15 @@
+using System;
 using DG.Tweening;
-using Internal.Scripts.Extensions;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
-namespace Internal.Scripts.InteractableObjects.Abstraction
+namespace Internal.Scripts.InteractableObjects
 {
-    public abstract class InteractableObject : MonoBehaviour, IClickable
+    public class InteractableObject : MonoBehaviour
     {
+        public event Action<InteractableObject> OnClick;
+
         [Header("Visual")]
         [SerializeField] protected SpriteRenderer _spriteRenderer;
         [SerializeField] protected SpriteRenderer _outline;
@@ -21,55 +24,45 @@ namespace Internal.Scripts.InteractableObjects.Abstraction
         [SerializeField] protected Vector2 _size = new Vector2(2, 2);
 
         protected BoxCollider2D Collider;
-        
-        #region Editor
-        protected void Reset()
+
+        private void Awake()
+        {
+            _outline.color = _outline.color.WithAlpha(0f);
+        }
+
+#if UNITY_EDITOR
+        private void Reset()
         {
             _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
             _textMesh = GetComponentInChildren<TextMeshPro>();
             Collider = GetComponent<BoxCollider2D>();
         }
 
-        protected void OnValidate()
+        private void OnValidate()
         {
             if (_spriteRenderer == null || _textMesh == null || Collider == null)
             {
                 Reset();
             }
-
             ApplyChanges();
         }
 
-        protected void ApplyChanges()
+        private void ApplyChanges()
         {
             _spriteRenderer.size = _size;
             _outline.size = _size;
             Collider.size = _size;
-            
-            _textMesh.text = _interactableName;
 
+            _textMesh.text = _interactableName;
             _textMesh.transform.localPosition = new Vector3(0, _size.y / 2 + _textOffsetY, 0);
             _textMesh.enableAutoSizing = true;
             _textMesh.fontSizeMin = 1;
             _textMesh.fontSizeMax = 512;
-
-            RectTransform rectTransform = _textMesh.rectTransform;
-            rectTransform.sizeDelta = new Vector2(_size.x * _textSizeModifier.x, _size.y * _textSizeModifier.y); 
-
+            _textMesh.rectTransform.sizeDelta = new Vector2(_size.x * _textSizeModifier.x, _size.y * _textSizeModifier.y);
             _textMesh.alignment = TextAlignmentOptions.Bottom;
         }
-        #endregion
-
-        protected virtual void Awake()
-        {
-            _outline.color = _outline.color.GetColorWithAlpha(0f);
-        }
-
-        public virtual void OnClick()
-        {
-            Debug.Log($"{_interactableName} clicked!");
-        }
-
+#endif
+        
         public void SwitchObjectState(bool state)
         {
             SwitchHeaderState(state);
@@ -78,7 +71,7 @@ namespace Internal.Scripts.InteractableObjects.Abstraction
 
         public void SwitchHeaderState(bool state)
         {
-            _textMesh.DOColor(_textMesh.color.GetColorWithAlpha(state ? 1f : 0f), 0.3f);
+            _textMesh.DOColor(_textMesh.color.WithAlpha(state ? 1f : 0f), 0.3f);
         }
 
         public void SwitchInputState(bool canClick)
@@ -86,14 +79,16 @@ namespace Internal.Scripts.InteractableObjects.Abstraction
             Collider.enabled = canClick;
         }
 
-        public void OnHoverEnter()
+        public void TriggerClick() => OnClick?.Invoke(this);
+
+        public void TriggerHoverEnter()
         {
-            _outline.DOColor(_outline.color.GetColorWithAlpha(1f), 0.3f);
+            _outline.DOColor(_outline.color.WithAlpha(1f), 0.3f);
         }
 
-        public void OnHoverExit()
+        public void TriggerHoverExit()
         {
-            _outline.DOColor(_outline.color.GetColorWithAlpha(0f), 0.3f);
+            _outline.DOColor(_outline.color.WithAlpha(0f), 0.3f);
         }
     }
 }
