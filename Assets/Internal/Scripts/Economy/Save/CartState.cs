f368ffab534c3a4d18407e6cb33afccf1235a7e7
D9eka <120364200+D9eka@users.noreply.github.com>
2026-02-07 20:41:07 +0500

    Создание PlayerResourceRepository
    - PlayerResourceRepository будет хранить все показатели игрока.
    - Удаление показателя максимального веса из системы инвентаря

---
+++ Assets/Internal/Scripts/Economy/Save/CartState.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Internal.Scripts.Economy.Save
+{
+    [Serializable]
+    public class CartState
+    {
+        public float Durability = 100f;
+        public float Capacity = 50f;
+    }
+}
