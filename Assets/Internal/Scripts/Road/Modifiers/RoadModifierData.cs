99f49aefde22cfa1b2af5859209add4ae69c2bd4
D9eka <120364200+D9eka@users.noreply.github.com>
2026-02-14 00:27:16 +0500

    Добавление NPC города и позиционирования на дороге
     NPC в детальных сценах городов. Расчёт позиций сбоку от дороги. Модификаторы дороги.

---
+++ Assets/Internal/Scripts/Road/Modifiers/RoadModifierData.cs
@@ -0,0 +1,36 @@
+using Internal.Scripts.UI.Localization;
+using Internal.Scripts.UI.Tooltip;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Internal.Scripts.Road.Modifiers
+{
+    [CreateAssetMenu(menuName = "SPJ/Road/Modifier", fileName = "RoadModifier")]
+    public class RoadModifierData : LocalizedTooltipData
+    {
+        [field: SerializeField] public string Id { get; private set; }
+        [field: SerializeField] public LocalizedString Name { get; private set; } = new();
+        [field: SerializeField] public LocalizedString Description { get; private set; } = new();
+        [field: SerializeField] public Sprite Icon { get; private set; }
+
+        protected override LocalizedString TooltipName => Name;
+        protected override LocalizedString TooltipDescription => Description;
+        protected override string TooltipId => Id;
+        protected override string TooltipContext => "RoadModifier";
+        protected override string FallbackDescription => "Road effect";
+
+#if UNITY_EDITOR
+        public void ApplyImport(
+            string id,
+            LocalizedString name,
+            LocalizedString description,
+            string iconPath)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            // TODO: Icon = load from Resources or addressables using iconPath
+        }
+#endif
+    }
+}
