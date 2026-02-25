98cb15eba4079b646044d43386eb196100328138
D9eka <120364200+D9eka@users.noreply.github.com>
2026-02-25 15:57:15 +0500

    Обновление импорта данных
    - Обновление данных городов
    - Удаление импорта биомов из csv (импортируется из blender)

--- Assets/Internal/Scripts/Economy/Generated/Biome.gen.cs
+++ Assets/Internal/Scripts/Economy/Generated/Biome.gen.cs
@@ -1,14 +1,10 @@
-namespace Internal.Scripts.Economy.Generated
+﻿namespace Internal.Scripts.Economy.Generated
 {
     public enum Biome
     {
         Unknown = 0,
         Plains = 1,
         Desert = 2,
-        Mountain = 3,
-        Forest = 4,
-        Coast = 5,
-        RiverValley = 6,
-        HighAltitude = 7,
+        HighAltitude = 3,
     }
 }
--- Assets/Internal/Scripts/Import/Editor/Theme/BiomePalettesTable.cs
+++ Assets/Internal/Scripts/Import/Editor/Theme/BiomePalettesTable.cs
@@ -20,6 +20,8 @@ namespace Internal.Scripts.Import.Editor.Theme
             "OverlayColor", "IconPrimary", "IconSecondary", "TimeButtonBackground",
             "SliderBackground", "SliderFillPositive", "SliderFillNegative",
             "ButtonSelectedBorder",
+            "RoadHighlight", "ValuePositive", "ValueNegative", "AccentHighlight",
+            "CityMarker",
         };
 
         public static List<(Biome biome, UiColorPalette palette)> Import(
@@ -85,7 +87,9 @@ namespace Internal.Scripts.Import.Editor.Theme
                     colors[0], colors[1], colors[2], colors[3],
                     colors[4], colors[5], colors[6], colors[7],
                     colors[8], colors[9], colors[10], colors[11],
-                    colors[12], colors[13], colors[14], colors[15]);
+                    colors[12], colors[13], colors[14], colors[15],
+                    colors[16], colors[17], colors[18], colors[19],
+                    colors[20]);
 
                 EditorUtility.SetDirty(palette);
                 result.Add((biome, palette));
