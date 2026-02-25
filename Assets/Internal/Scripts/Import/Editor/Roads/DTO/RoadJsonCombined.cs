f1cd7e134143b72c9f038e91d1721a16a018268d
D9eka <120364200+D9eka@users.noreply.github.com>
2026-02-25 15:55:00 +0500

    Добавление функции покраски дорог

--- Assets/Internal/Scripts/Import/Editor/Roads/DTO/RoadJsonCombined.cs
+++ Assets/Internal/Scripts/Import/Editor/Roads/DTO/RoadJsonCombined.cs
@@ -3,11 +3,19 @@ using System;
 namespace Internal.Scripts.Import.Editor.Roads.DTO
 {
     [Serializable]
+    public class NodeJson
+    {
+        public string NodeId;
+        public string Biome;
+    }
+
+    [Serializable]
     public class RoadJsonCombined
     {
         public int Version;
         public string RelativeTo;
         public string Space;
         public RoadJsonSingle[] Roads;
+        public NodeJson[] Nodes;
     }
 }
